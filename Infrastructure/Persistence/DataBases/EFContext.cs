7c95673c58784b56ddcf6704b27715cd8b68d43f
Huseyn Haydarov <huseynhaydarov0@gmail.com>
2024-05-31 22:37:07 +0500

    Added connection string

---
+++ Infrastructure/Persistence/DataBases/EFContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.DataBases;
+
+public class EFContext : DbContext
+{
+    public EFContext(DbContextOptions<EFContext> options) : base(options)
+    {
+
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=BSM;Username=postgres;Password=7878_Postgresql");
+        base.OnConfiguring(optionsBuilder);
+    }
+}
