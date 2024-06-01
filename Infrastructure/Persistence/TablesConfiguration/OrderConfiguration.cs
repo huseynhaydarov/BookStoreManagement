dddb30b9391300b08b27275e78c4a475930e6ca4
Huseyn Haydarov <huseynhaydarov0@gmail.com>
2024-06-01 16:34:58 +0500

    Added migrations

--- Infrastructure/Persistence/TablesConfiguration/OrderConfiguration.cs
+++ Infrastructure/Persistence/TablesConfiguration/OrderConfiguration.cs
@@ -16,7 +16,9 @@ public class OrderConfiguration : IEntityTypeConfiguration<Order>
     {
         builder.HasKey(o => o.Id);
         builder.Property(o => o.OrderDate).IsRequired();
-        builder.Property(o => o.Status).HasConversion<OrderStatus>().IsRequired();
+        builder.Property(o => o.Status).HasConversion(
+            o => o.ToString(),
+            o => (OrderStatus)Enum.Parse(typeof(OrderStatus), o));
         builder.Property(o => o.TotalAmount).HasPrecision(18, 2).IsRequired();
         builder.HasMany(o => o.Items).WithOne(o => o.Order).HasForeignKey(o => o.OrderId);
     }
