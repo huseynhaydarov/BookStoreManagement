bc0982576a496f238cfae7626737706a37078e97
Huseyn Haydarov <huseynhaydarov0@gmail.com>
2024-06-01 23:08:23 +0500

    Added Book Controller

---
+++ Web.API/ApiEndpoints.cs
@@ -0,0 +1,17 @@
+﻿namespace Web.API
+{
+    public static class ApiEndpoints
+    {
+        private const string ApiBase = "api";
+
+        public static class Book
+        {
+            public const string Base = $"{ApiBase}/book";
+            public const string Create = Base;
+            public const string Get = $"{Base}/{{id:int}}";
+            public const string GetAll = Base;
+            public const string Update = $"{Base}/{{id:int}}";
+            public const string Delete = $"{Base}/{{id:int}}";
+        }
+    }
+}
