71d419aea29b2215b94490489ef1d67409c88ca3
Huseyn Haydarov <huseynhaydarov0@gmail.com>
2024-07-05 14:26:23 +0500

    Implemented In memory caching

--- Infrastructure/Persistence/Repositories/BookRepository.cs
+++ Infrastructure/Persistence/Repositories/BookRepository.cs
@@ -1,6 +1,9 @@
 ﻿using Application.Common.Interfaces.Repositories;
+using Contracts.Pagination;
+using Domain.Abstract;
 using Domain.Entities;
 using Infrastructure.Persistence.DataBases;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Persistence.Repositories;
 
